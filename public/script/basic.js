e1087cb1a779ebf9553e802dd26d2a66a9573c96
chirsen <1020269294@qq.com>
2015-12-13 17:55:37 +0800

    初步完成基本功能，还需要完善分页和按键弹起查询

--- public/script/basic.js
+++ public/script/basic.js
@@ -12,7 +12,8 @@ $(document).ready(function(){
 		var choose_a = $("a").filter(function(index){
 			return $(this).css("fontWeight")=="bold";
 		});
-		$.getJSON("/upload", {"name":text, "choose" : choose_a.attr("href")}, function(result){
+		$.getJSON("/upload", {"text":text, "choose" : choose_a.attr("href")}, function(result){
+			$(".container").html("");
 			$.each(result, function(i, field){
        			$(".container").append("<div class='item'><p>名字"+field.name+"</p><p>链接：<a target='_blank' href="+field.link+">"+field.link+"</a></p></div>");
      		});
--- server.js
+++ server.js
@@ -9,57 +9,89 @@ var mysql = require('mysql');
 
 var app = express();
 var pool = mysql.createPool({
-	connectionLimit:10,
-	host  : 'localhost',
-	user  : 'root',
-	password:'q',
-	database:'professor'
+	connectionLimit: 10,
+	host: 'localhost',
+	user: 'root',
+	password: 'q',
+	database: 'reachers'
 });
 
 
 app.use(express.static('public'));
 
-app.get('/reserchersFinder', function(req, res){
+app.get('/reserchersFinder', function(req, res) {
 	var pathname = url.parse(req.url).pathname;
-	res.sendFile(__dirname+"/"+"index.html");
+	res.sendFile(__dirname + "/" + "index.html");
 	console.log("接受：");
 });
 
-
-app.get("/upload", function(req, res){
-	pool.getConnection(function(err, connction){
-		if(err){
+//根据输入框的内容和选择的choose来进行搜索
+app.get("/upload", function(req, res) {
+	pool.getConnection(function(err, connction) {
+		if (err) {
 			console.log("连接错误");
 			throw err;
 		}
-		connction.query('select * from ccnu where id < 3', function(error, rows){
+		//获取输入框和选择的内容
+		var text = req.query.text;
+		var choose = req.query.choose;
+		console.log("提交的文字：" + text + "选择的choose: " + choose);
+		//根据选择的内容进行相应的查询
+		var tab;
+		switch (Number(choose)) {
+			case 1:
+				tab = "name";
+				break;
+			case 2:
+				tab = "school";
+				break;
+			case 3:
+				tab = "direction";
+				break;
+			case 4:
+				tab = "position";
+				break;
+		}
+		//对传送过来的输入框中的内容进行处理，以便进行模糊查询
+		var myCars = new Array();
+		myCars[0] = "%";
+		for (var i = 0; i < text.length; i++) {
+			myCars[i + 1] = text.charAt(i) + "%";
+		}
+		text = myCars.join("");
+		//log中打印拼接后的字符串
+		console.log("拼接之后的text: " + text);
 
+		//根据输入的字符串和选择进行查询：
+		var query = "select * from ccnu where " + tab + " like '" + text + "' limit 10";
+		//打印查询语句：
+		console.log(query);
+		connction.query(query, function(error, rows) {
 			var professors = new Object();
-			for(var i = 0; i < rows.length; i++){
+			for (var i = 0; i < rows.length; i++) {
 				var professor = new Object();
 				professor.name = rows[i].name;
 				professor.direction = rows[i].direction;
-				professor.level = rows[i].level;
-				professor.school  = rows[i].school;
-				professor.link  = rows[i].link;
+				professor.position = rows[i].position;
+				professor.school = rows[i].school;
+				professor.link = rows[i].link;
 				professors[i] = professor;
 			}
 
 			console.log(professors);
 			connction.release();
 			var text = JSON.stringify(professors);
-			console.log("得到的JSON字符串："+text);
-			console.log("转换得到的对象: "+(JSON.parse(text))[0].name);
+			console.log("得到的JSON字符串：" + text);
 			res.send(JSON.stringify(professors));
+		});
 
-		})
 	});
-	
+
 });
 
-var server = app.listen(8081, function(){
+var server = app.listen(8081, function() {
 	var host = server.address().address;
 	var port = server.address().port;
 
-	console.log(host+" "+port);
+	console.log(host + " " + port);
 });
