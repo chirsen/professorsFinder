4d577bf941891b41ae193a34e4d9f6ed8b3ff16e
chirsen <1020269294@qq.com>
2015-12-14 08:03:18 +0800

    分页功能待完善

--- public/script/basic.js
+++ public/script/basic.js
@@ -3,59 +3,123 @@
 	$("a:first").css({"textDecoration":"#f00 solid underline", "fontWeight", "blod"});
 });
 */
-$(document).ready(function(){
-	$("a:first").css({"fontWeight":"bold", "text-decoration":"underline"});
-	
-	//上传请求到服务器的函数
-	var upload = function(){
-		var text = $("#search_input").val();
-		var choose_a = $("a").filter(function(index){
-			return $(this).css("fontWeight")=="bold";
-		});
-		$.getJSON("/upload", {"text":text, "choose" : choose_a.attr("href")}, function(result){
-			$(".container").html("");
-			$.each(result, function(i, field){
-       			$(".container").append("<div class='item'><p>名字: "
-       				+field.name+"</p><p>链接：<a onclick='uploadid("
-       					+field.id+")' target='_blank' href="
-       				+field.link+">"
-       				+field.link+"</a></p></div>");
-     		});
-		});
-	};
+$(document).ready(function() {
+	$("a:first").css({
+		"fontWeight": "bold",
+		"text-decoration": "underline"
+	});
 
+
+	$(".page_item2").click(function() {
+		alert()
+	});
 	//当连接被点击
-	$("a").click(function(){
+	$("a").click(function() {
+		$("a").css({
+			"fontWeight": "normal",
+			"text-decoration": "none"
+		});
+		$(this).css({
+			"fontWeight": "bold",
+			"text-decoration": "underline"
+		});
 		var text = $("#search_input").val();
-		$("a").css({"fontWeight":"normal", "text-decoration":"none"});
-		$(this).css({"fontWeight":"bold", "text-decoration":"underline"});
-		if(text){
-			$("body").css({"top": "0px", "left": "10%"});
-			upload();
+		var choose_a = $("a").filter(function(index) {
+			return $(this).css("fontWeight") == "bold";
+		});
+		if (text) {
+			$("body").css({
+				"top": "0px",
+				"left": "10%"
+			});
+			upload(text, choose_a, 1);
 		}
 		return false;
 	});
 
 	//当搜索按钮被点击
-	$("#search_button").click(function(){
-		$("body").css({"top":"0px", "left":"10%"});
+	$("#search_button").click(function() {
+		$("body").css({
+			"top": "0px",
+			"left": "10%"
+		});
 		$(this).css("background-color", "#6079D5");
-		upload();
+		var text = $("#search_input").val();
+		var choose_a = $("a").filter(function(index) {
+			return $(this).css("fontWeight") == "bold";
+		});
+		upload(text, choose_a, 1);
 	});
-	$("#search_button").mouseover(function(){
+	$("#search_button").mouseover(function() {
 		$(this).css("background-color", "#6079D5");
 	});
-	$("#search_button").mouseout(function(){
+	$("#search_button").mouseout(function() {
 		$(this).css("background-color", "#647ed9");
 	});
 
 	//当按下回车
-	$("#search_input").keypress(function(e){
-		if(e.which == 13){
-			$("body").css({"top": "0px", "left": "10%"});
-			upload();
+	$("#search_input").keypress(function(e) {
+		if (e.which == 13) {
+			$("body").css({
+				"top": "0px",
+				"left": "10%"
+			});
+			var text = $("#search_input").val();
+			var choose_a = $("a").filter(function(index) {
+				return $(this).css("fontWeight") == "bold";
+			});
+			upload(text, choose_a, 1);
 		}
 	});
 
 
 });
+
+//上传请求到服务器的函数
+var upload = function(text, choose_a, currentPage) {
+	$.getJSON("/upload", {
+		"text": text,
+		"choose": choose_a.attr("href"),
+		"currentPage": currentPage
+	}, function(result) {
+		$(".container").html("");
+		$.each(result.professors, function(i, field) {
+			//设置内容块儿的显示数据
+			$(".container").append("<div class='item'><p>名字: " + field.name + "<p>所在院校：" + field.school + "</p><p>职称：" + field.position + "</p></p><p>链接：<a onclick='uploadid(" + field.id + ")' target='_blank' href=" + field.link + ">" + field.link + "</a></p></div>");
+		});
+
+		$(".container").append("<div class='pages'></div>");
+		//如果总的页数小于或者等于1
+		if (result.totalPages > 1 && result.totalPages <= 10) {
+			if (result.currentPage != '1') {
+				$(".pages").append("<a class='page_item1' href='#' onclick='upload(" + text + "," + choose_a + "," + (Number(result.currentPage) - 1) + "); return false;' left='0px'>上一页</a>");
+			}
+			for (var i = 0; i < result.totalPages; i++) {
+				$(".pages").append("<a class = 'page_item2' href='#' href ='#' onclick='upload(" + text + "," + choose_a + "," + (i + 1) + "); return false;' left='0px'>" + (i + 1) + "</a>");
+			}
+			$(".pages").append("<a class='page_item1' href='#'  onclick='upload(" + text + "," + choose_a + "," + (Number(result.currentPage) + 1) + "); return false;'>下一页</a>");
+		} else if (result.totalPages > 10) {
+			if (result.currentPage != '1') {
+				$(".pages").append("<a class='page_item1' href='#'  onclick='upload(" + text + "," + choose_a + "," + (Number(result.currentPage) - 1) + "); return false;' left='0px'>上一页</a>");
+			}
+			if (result.currentPage <= 5) {
+				for (var i = 0; i < 10; i++) {
+					$(".pages").append("<a class = 'page_item2' href='#'  onclick='upload(" + text + "," + choose_a + "," + (i + 1) + "); return false;' left='0px'>" + (i + 1) + "</a>");
+				}
+			} else {
+				for (var i = 0; i < 10; i++) {
+					$(".pages").append("<a class='page_item2' href='#'  onclick='upload(" + text + "," + choose_a + "," + (result.currentPage - 4 + i) + "); return false;' left='0px'>" + (result.currentPage - 4 + i) + "</a>");
+				}
+			}
+			$(".pages").append("<a class='page_item1' href='#' onclick='upload(" + text + "," + choose_a + "," + (Number(result.currentPage) + 1) + "); return false;'  >下一页</a>");
+		} else {
+			$(".pages").append("没有更多的数据");
+		}
+	});
+};
+
+function uploadid(id) {
+	$.get("/uploadid", {
+		"id": id
+	});
+}
--- server.js
+++ server.js
@@ -35,9 +35,10 @@ app.get("/upload", function(req, res) {
 			console.log("连接错误");
 			throw err;
 		}
-		//获取输入框和选择的内容
+		//获取输入框和选择的内容并且获取分页的信息
 		var text = req.query.text;
 		var choose = req.query.choose;
+		var currentPage = req.query.currentPage;
 		console.log("提交的文字：" + text + "选择的choose: " + choose);
 		//根据选择的内容进行相应的查询
 		var tab;
@@ -68,15 +69,23 @@ app.get("/upload", function(req, res) {
 		//根据输入的字符串和选择进行查询：
 		var query;
 		if(text == ""){
-			query = "select * from ccnu order by hot desc limit 10";	
+			query = "select * from ccnu order by hot limit 10";	
 		}else{
-			query = "select * from ccnu where " + tab + " like '" + text + "' order by hot desc limit 10";
+			query = "select * from ccnu where " + tab + " like '" + text + "' order by hot";
 		}
 		//打印查询语句：
 		console.log(query);
 		connction.query(query, function(error, rows) {
+			//获取查询的数量
+			var rowsSize = rows.length;
+			//需要传递给页面的数量
+			var start = Number(currentPage-1)*10;
+			var end = ((rowsSize - start)>=10)?(start+10):(rowsSize);
+			//构建要传递的对象（当前页，传递给当前页的数据）
+			console.log("开头页码和结尾页码："+start+" "+end);
 			var professors = new Object();
-			for (var i = 0; i < rows.length; i++) {
+			var j = 0;
+			for (var i = start; i < end; i++) {
 				var professor = new Object();
 				professor.id = rows[i].id;
 				professor.name = rows[i].name;
@@ -84,13 +93,14 @@ app.get("/upload", function(req, res) {
 				professor.position = rows[i].position;
 				professor.school = rows[i].school;
 				professor.link = rows[i].link;
-				professors[i] = professor;
+				professors[j++] = professor;
+				console.log(rows[i].name);
 			}
-
-			console.log(professors);
+			var result = {"currentPage":currentPage, "totalPages":(rowsSize/10), "professors": professors};
+			console.log(result);
 			connction.release();
 			var text = JSON.stringify(professors);
-			res.send(JSON.stringify(professors));
+			res.end(JSON.stringify(result));
 		});
 
 	});
